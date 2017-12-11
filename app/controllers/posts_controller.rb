75eb849569de946ab74c4dc200c1a3345c87ac0f
Jaja Pabatao <headshot99001@gmail.com>
2017-12-11 16:03:51 +0800

    fixed some indention

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,56 +1,56 @@
-class PostsController < ApplicationController
-	before_action :find_post, only: [:show, :update, :edit, :destroy]
+  class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :update, :edit, :destroy]
 
-def index
-	@post = Post.all.order("created_at DESC")
-end
+  def index
+    @post = Post.all.order("created_at DESC")
+  end
 
-def new
-	@post = Post.new
-end
+  def new
+    @post = Post.new
+  end
 
-def create
-	@post = Post.new(post_params)
-	if @post.save 
-		redirect_to @post
-	else
-		render 'new'
-	end
-end
+  def create
+    @post = Post.new(post_params)
+    if @post.save 
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
 
-def show
-end
+  def show
+  end
 
-def update
-	
+  def update
+    
 
-	if @post.update(post_params)	
-		redirect_to @post
-	else
-		render 'edit'
-	end 
+    if @post.update(post_params)  
+    redirect_to @post
+    else
+    render 'edit'
+    end 
 
-end
-def edit
+  end
+  def edit
 
-end
+  end
 
-def destroy
-	
-	@post.destroy
-redirect_to posts_path
-end
+  def destroy
+    
+    @post.destroy
+  redirect_to posts_path
+  end
 
 
-private
+  private
 
-def post_params
-	params.require(:post).permit(:title, :content)
-end
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
 
-def find_post
-	@post = Post.find(params[:id])
-	
-end
+  def find_post
+    @post = Post.find(params[:id])
+    
+  end
 
-end
+  end
